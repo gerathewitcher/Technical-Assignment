e48de694822e25363455fbf9a024471c39d62586
gerathewitcher <gerandokov1337@gmail.com>
2021-04-11 15:55:42 +0300

    created urls.py

--- main/api_views.py
+++ main/api_views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from .models import News
-from .serlializers import NewsSerializer
+from .serializers import NewsSerializer
+
 
 class NewsAPIView(viewsets.ModelViewSet):
     queryset = News.objects.all()
