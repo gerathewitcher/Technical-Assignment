f5065d472e29d5c687baeae4c596825fcb3e7112
gerathewitcher <gerandokov1337@gmail.com>
2021-04-11 15:22:24 +0300

    created News model and superuser

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import News
 
 # Register your models here.
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'created', 'published']
+    list_filter = ('id',)
+    search_fields = ('title',)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class News(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    published = models.DateTimeField()
+
+    class Meta:
+        verbose_name_plural = 'News'
+
+    def __str__(self):
+        return f'{self.id}.{self.title}'
